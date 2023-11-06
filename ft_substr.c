efbc408d5e5824891bd27c045c32cc8f72f3de17
asmae <asmasabirpro@gmail.com>
2023-11-06 20:25:17 +0100

    substr

--- ft_substr.c
+++ ft_substr.c
@@ -1,5 +1,27 @@
 #include <stdlib.h>
 char *ft_substr(char const *s, unsigned int start, size_t len)
 {
-    
+    char * p;
+    size_t i;
+
+    i = 0;
+    p = (char *)malloc((len+1)*sizeof(char));
+    if (!p)
+    {
+        return (NULL);
+    }
+    while (s[start + i]!='\0' && i < len)
+    {
+        p[i] = s[start + i];
+        i++;
+    }
+    p[i] = '\0';
+    return (p);
+}
+#include <stdio.h>
+int main ()
+{
+    char const chr[]= "hello world";
+    char * res = ft_substr(chr,2, 3);
+    printf("%s",res);
 }
